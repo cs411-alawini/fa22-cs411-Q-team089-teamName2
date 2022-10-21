79be50be3d8830c78a1d5a3d67d3652ad0abb658
Shlok Mehrotra <shlokmehrotra@gmail.com>
2022-10-21 18:30:41 -0500

    schema updates

--- schemas/schemas.sql
+++ schemas/schemas.sql
@@ -2,41 +2,49 @@ CREATE DATABASE IF NOT EXISTS cs411;
 USE cs411;
 
 
+CREATE TABLE IF NOT EXISTS Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
+
 CREATE TABLE IF NOT EXISTS User (
     username VARCHAR(255),
     passwords VARCHAR(255),
     names VARCHAR(255),
     completionRate FLOAT,
-    PRIMARY KEY (username),
+    PRIMARY KEY (username)
 );
 
 
--- CREATE TABLE IF NOT EXISTS Checklist (
---     checkListId INT,
---     names VARCHAR(255),
---     username VARCHAR(255),
---     FOREIGN KEY TO User.username,
---     PRIMARY KEY (checkListId)
--- );
+CREATE TABLE IF NOT EXISTS Checklist (
+    checkListId INT,
+    names VARCHAR(255),
+    username VARCHAR(255),
+    FOREIGN KEY TO User.username,
+    PRIMARY KEY (checkListId)
+);
 
--- CREATE TABLE IF NOT EXISTS FriendGroup (
---     groupId INT,
---     names VARCHAR(255),
---     PRIMARY KEY (groupId)
--- );
+CREATE TABLE IF NOT EXISTS FriendGroup (
+    groupId INT,
+    names VARCHAR(255),
+    PRIMARY KEY (groupId)
+);
 
--- CREATE TABLE IF NOT EXISTS Alert (
---     alertId INT,
---     messages VARCHAR(255),
---     pingedUser VARCHAR(255),
---     FOREIGN KEY TO User.username
--- );
+CREATE TABLE IF NOT EXISTS Alert (
+    alertId INT,
+    messages VARCHAR(255),
+    pingedUser VARCHAR(255),
+    FOREIGN KEY TO User.username
+);
 
--- CREATE TABLE IF NOT EXISTS Tasks(
---     taskId INT PRIMARY KEY,
---     checkListId INT,
---     dateCompleted DATE,
---     taskContent VARCHAR(255),
---     FOREIGN KEY TO Checklist.checklistId,
---     PRIMARY KEY (taskId)
--- );
+CREATE TABLE IF NOT EXISTS Tasks(
+    taskId INT PRIMARY KEY,
+    checkListId INT,
+    dateCompleted DATE,
+    taskContent VARCHAR(255),
+    FOREIGN KEY TO Checklist.checklistId,
+    PRIMARY KEY (taskId)
+);
