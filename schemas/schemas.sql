af2f6835cd108ef532323653360749052e18db93
Shlok Mehrotra <shlokmehrotra@gmail.com>
2022-10-20 23:02:48 -0500

    git schemas

--- schemas/schemas.sql
+++ schemas/schemas.sql
@@ -1,38 +1,42 @@
--- (username:VARCHAR(255) [PK], password:VARCHAR(255), name:VARCHAR(255), completionRate:FLOAT)
-CREATE TABLE User IF NOT EXIST(
-    username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255),
-    name VARCHAR(255),
-    completionRate FLOAT
-);
+CREATE DATABASE IF NOT EXISTS cs411;
+USE cs411;
 
--- Checklist(checkListId:INT [PK], name:VARCHAR(255), username:VARCHAR(255) [FK to User.username])
-CREATE Checklist IF NOT EXIST(
-    checkListId INT PRIMARY KEY,
-    name VARCHAR(255),
+
+CREATE TABLE IF NOT EXISTS User (
     username VARCHAR(255),
-    FOREIGN KEY TO User.username
+    passwords VARCHAR(255),
+    names VARCHAR(255),
+    completionRate FLOAT,
+    PRIMARY KEY (username),
 );
 
--- FriendGroup(groupId:INT [PK], name: VARCHAR(255))
-CREATE FriendGroup IF NOT EXIST(
-    groupId INT PRIMARY KEY,
-    name VARCHAR(255)
-);
 
--- Alert(alertId:INT [PK], Message: VARCHAR(255), pingedUser:VARCHAR(255) [FK to User.username])
-CREATE Alert IF NOT EXIST(
-    alertId INT,
-    message VARCHAR(255),
-    pingedUser VARCHAR(255),
-    FOREIGN KEY TO User.username
-);
+-- CREATE TABLE IF NOT EXISTS Checklist (
+--     checkListId INT,
+--     names VARCHAR(255),
+--     username VARCHAR(255),
+--     FOREIGN KEY TO User.username,
+--     PRIMARY KEY (checkListId)
+-- );
 
--- Task(taskId:INT [PK], checklistID:INT [FK to Checklist.checklistId], dateCompleted:DATE, taskContent:VARCHAR(255))
-CREATE Task IF NOT EXIST(
-    taskId INT PRIMARY KEY,
-    checkListId INT,
-    dateCompleted DATE,
-    taskContent VARCHAR(255),
-    FOREIGN KEY TO Checklist.checklistId
-);
+-- CREATE TABLE IF NOT EXISTS FriendGroup (
+--     groupId INT,
+--     names VARCHAR(255),
+--     PRIMARY KEY (groupId)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS Alert (
+--     alertId INT,
+--     messages VARCHAR(255),
+--     pingedUser VARCHAR(255),
+--     FOREIGN KEY TO User.username
+-- );
+
+-- CREATE TABLE IF NOT EXISTS Tasks(
+--     taskId INT PRIMARY KEY,
+--     checkListId INT,
+--     dateCompleted DATE,
+--     taskContent VARCHAR(255),
+--     FOREIGN KEY TO Checklist.checklistId,
+--     PRIMARY KEY (taskId)
+-- );
