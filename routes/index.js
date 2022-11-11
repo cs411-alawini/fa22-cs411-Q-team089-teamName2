d7bfdb92b5ec9e4b7253a40f62c92ba2d9d0b20d
Riki Terada <rterada123@gmail.com>
2022-11-10 18:11:17 -0600

    Created Landing Page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require("path");
+const db = require("../database.js")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Mark Attendance' });
+});
+
+router.get('/mark', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./database.js')
+const app = express();
+
+// set up ejs view engine 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+ 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '../public'));
+
+// Apply routers
+app.use('/', require('./routes/index.js'))
+
+app.listen(80, function () {
+    console.log('Node app is running on port 80');
+});
