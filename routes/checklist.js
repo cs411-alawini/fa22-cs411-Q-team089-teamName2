e15a1778b521f452399f62ae197025abcb82a546
Riki Terada <rterada123@gmail.com>
2022-11-11 02:02:52 -0600

    Display checklist from database

--- routes/checklist.js
+++ routes/checklist.js
@@ -4,9 +4,18 @@ const path = require("path");
 const db = require("../database.js")
 
 router.get('/', function(req, res) {
-  var username = req.query.username
-  console.log(username)
-  res.render('checklist');
+  var username = req.query.username;
+  
+  var sql = `SELECT checkListId, names FROM Checklist WHERE userId=${username}`;
+  db.query(sql, function(err, result) {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    res.render("checklist", {
+      data: result
+    })
+  })
 });
 
 module.exports = router; 
