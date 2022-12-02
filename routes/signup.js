697f5a11887d8a6ce7cf000c3445cfaca8aa13f3
Riki Terada <rterada123@gmail.com>
2022-12-02 00:21:03 -0600

    Sign up page functioning

--- routes/signup.js
+++ routes/signup.js
@@ -2,29 +2,64 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 const db = require("../database.js")
+const fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('signup');
+var currUserId;
+var currChecklistId;
+
+fs.readFile('./id.json', 'utf8', function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    obj = JSON.parse(data);
+    currUserId = obj.userId;
+    currChecklistId = obj.checkListId;
+  }
 });
 
-router.post('/signup', function(req, res) {
-  var userId = req.body.userId
+router.get('/', function(req, res) {
+  currUserId+=1;
+  currChecklistId+=1;
+  res.render('signup', {userId: currUserId});
+});
+
+router.post('/createUser', function(req, res) {
+  var name = req.body.name
   var password = req.body.password
+  fs.readFile('./id.json', 'utf8', function(err, data) {
+    if (err) {
+      res.send(err);
+      return;
+    } else {
+      obj = JSON.parse(data);
+      obj.userID = currUserId;
+      obj.checkListId = currChecklistId;
+      json = JSON.stringify(obj)
+      fs.writeFile('./id.json', json, 'utf8', function(err) {
+        if (err) res.send(err);
+        return;
+      });
+    }
+  });
 
   var sql = `
-  SELECT userId, passwords FROM User WHERE userId=${userId}`;
+  INSERT INTO User VALUES (${currUserId}, '${password}', '${name}', 1)`;
   console.log(sql);
   db.query(sql, function(err, result) {
     if (err) {
       res.send(err);
       return;
     }
-    if (result.length>0 && result[0].passwords == password) {
-      res.redirect(`/checklist/landing?userId=${userId}`);
-    } else {
-      res.render('index', {success: false});
-    }
+    sql = `
+    INSERT INTO Checklist VALUES (${currChecklistId}, 'Archive', ${currUserId})`;
+
+    db.query(sql, function(err, result) {
+      if (err) {
+        res.send(err);
+        return;
+      }
+      res.redirect(`/checklist/landing?userId=${currUserId}`);
+    });
   });
 });
 
